dc2c45f729d65a80b548b952eec6664b464d73dd
SampsonSage <SampsonSage2024@outlook.com>
2024-09-23 02:21:35 -0400

    Update ComfyUI.sh

--- ComfyUI.sh
+++ ComfyUI.sh
@@ -18,10 +18,10 @@ main() {
     run_command "pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu124"
     run_command "pip install -r requirements.txt"
     run_command "cd models/clip && aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors -o clip_l.safetensors"
-    run_command "cd models/clip && aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors -o t5xxl_fp8_e4m3fn.safetensors"
-    run_command "cd models/clip && aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors -o t5xxl_fp16.safetensors"
-    run_command "cd models/vae && aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true --header=\"Authorization: Bearer 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\" https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors -o ae.safetensors"
-    run_command "cd models/unet && aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true --header=\"Authorization: Bearer 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\" https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors -o flux1-dev.safetensors"
+    run_command "aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors -o t5xxl_fp8_e4m3fn.safetensors"
+    run_command "aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors -o t5xxl_fp16.safetensors"
+    run_command "cd .. && cd vae && aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true --header=\"Authorization: Bearer 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\" https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors -o ae.safetensors"
+    run_command "cd .. && cd unet && aria2c -x16 -s16 -k1M --file-allocation=none --optimize-concurrent-downloads=true --header=\"Authorization: Bearer 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\" https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors -o flux1-dev.safetensors"
 
     echo "All tasks completed successfully!"
 }
